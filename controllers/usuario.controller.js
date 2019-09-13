bad2ff717991d481990ed7b5c57d09312e596a46
jpballejo <jballejo@gmail.com>
2019-09-13 17:48:30 -0300

    agregado abm al controlador de usuario, chet the boys...

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -1,13 +1,18 @@
+var Usuario = require('../models/usuario.modelo');
 
-var modeloUsuario = require('../models/usuario.modelo');
+/**
+ * [description]
+ * @param  {[type]}   req  [request]
+ * @param  {[type]}   res  [response]
+ * @param  {Function} next [function next()]
+ * @return {[json]}        [retorna viste un error si falla o sino un json con el objeto representado ya tu sabe...]
+ */
 
+exports.altaUsuario = function(req, res, next) {
 
+  var usuario = new Usuario(req.body);
 
-exports.altaUsuario = function(req, res, next){
-
-  var usuario = new modeloUsuario(req.body);
-
-  usuario.save(function(err,user) {
+  usuario.save(function(err, user) {
     if (err) {
       return next(err);
     } else {
@@ -16,3 +21,79 @@ exports.altaUsuario = function(req, res, next){
 
   });
 }
+
+//agregar cascade:true
+/**
+ * [listaUsuario description]
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
+exports.listaUsuario = (req, res, next) => {
+  Usuario.find({}, (err, user) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.json(user);
+    }
+
+  });
+
+  //separar mas esto, definir una clase mas que contiene los metodos de los modelos y los ejecuta recibiendo o devolviendo parametros
+
+};
+
+/**
+ * [getUsuario description]
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
+exports.getUsuario = (req, res, next) => {
+  Usuario.findById(id, (err, user) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.json(user);
+    }
+
+  })
+
+
+};
+/**
+ * [updateUsuario description]
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
+exports.updateUsuario = (req, res, next) => {
+  Usuario.findByIdAndUpdate(req.usuario.id, req.body, (err, usuario) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.json(user);
+    }
+
+  });
+
+};
+/**
+ * [removeUsuario description]
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
+exports.removeUsuario=(req,res,next)=>{
+req.usuario.remove((err)=>{
+  if(err){return next(err);}
+  else{res.json(req.usuario);}
+});
+
+};
+
+//usar modificadores para los modelos 
